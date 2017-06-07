5b00c689e6d31d0f60b157b30f27bb826fbeb2c7
Paul Sutherland <paul@liquidbinary.co.uk>
2017-06-07 09:40:37 +0100

    fix(app) add back electron and path
    In order to talk with the file system and electron, you require your node packages in index.html and then declare them in your page classes.
    
    This was in previously, but must have been knocked out when upgrading Ionic versionsupgrading Ionic versions

--- src/src/pages/home/home.ts
+++ src/src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare const electron
+declare const path
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +11,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    console.log('Electron is now available: ', electron)
+    console.log('Electron remote is now available: ', electron.remote)
+    console.log('Path is now available: ', path)
   }
 
 }
