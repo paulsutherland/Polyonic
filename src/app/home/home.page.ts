1074559237ec08c9e5183b2a2b2426fbfd96f7c2
Paul Sutherland <paul@liquidbinary.co.uk>
2018-10-08 16:13:14 +0100

    feat: add ngx-electron

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  constructor(private electron: ElectronService) {
+    if (this.electron.isElectronApp) {
+      console.log('Running Electron:', this.electron);
+    } else {
+      console.log('Mode web');
+    }
+  }
+
+}
