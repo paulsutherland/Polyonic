ded9f9bb85dafe0a1ca0b625a9fdc4ca48eac5cd
Paul Sutherland <paul@liquidbinary.co.uk>
2019-02-18 09:27:21 +0000

    feat: enable and disable deploy
    to allow the app to work with ionic app flow

---
+++ config/disable-deploy.js
@@ -0,0 +1,14 @@
+// Allow angular using electron module (native node modules)
+const fs = require('fs')
+const config = 'config.xml'
+
+fs.readFile(config, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err)
+  }
+  var result = data.replace(`<preference name="DisableDeploy" value="false" />`, `<preference name="DisableDeploy" value="true" />`)
+
+  fs.writeFile(config, result, 'utf8', function (err) {
+    if (err) return console.log(err)
+  })
+})
